95ec96dc99925265ee05e02f1931b0c58a759455
radyatamaa <43398711+radyatamaa@users.noreply.github.com>
2021-08-21 20:33:54 +0700

    add function export excel all laporan

--- application/views/Dashboard/laporan/laporan_transaksi.php
+++ application/views/Dashboard/laporan/laporan_transaksi.php
@@ -48,8 +48,31 @@
 		          
 		        </table>
 
-                <div class="row">
-            		<h3 class="panel-title">Total pendapatan : <?= $data['totalPendapatan'] ?></h3>
+            <div class="row">
+					<button class="btn btn-primary btn-round" onClick="fnExcelReport()">Export Excel</button>
             	</div>
             </div>
     </div>
+
+    <script>
+
+	function fnExcelReport()
+{
+  var filename = "laporan_transaksi"
+var tab_text = "<table border='2px'><tr>";
+var textRange;
+var j = 0;
+tab = document.getElementById('datatab');
+
+for (j = 0; j < tab.rows.length; j++) {
+  tab_text = tab_text + tab.rows[j].innerHTML + "</tr>";
+}
+
+tab_text = tab_text + "</table>";
+var a = document.createElement('a');
+var data_type = 'data:application/vnd.ms-excel';
+a.href = data_type + ', ' + encodeURIComponent(tab_text);
+a.download = filename + '.xls';
+a.click();
+}
+</script>
